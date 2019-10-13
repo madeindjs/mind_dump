542851520339158eed7c25d24d2918a00ef1a0e4
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2019-10-13 11:18:50 +0200

    Install concrete

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,8 @@
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import 'normalize.css'
+import 'concrete.css'
 
 export default {
   name: 'app',
@@ -15,14 +17,3 @@ export default {
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
