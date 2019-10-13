814afa717f53adc4e72fc5430fdf1774ab5ad442
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2019-10-13 11:44:43 +0200

    create log

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,38 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <div class="row">
+      <div class="column">
+        <h1>Log Stuff</h1>
+        <CreateLog @create="onCreateLog"/>
+        <ul>
+          <li v-for="(log, index) in logs" :key="index">{{ log }}</li>
+        </ul>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 import 'normalize.css'
 import 'concrete.css'
 
+import CreateLog from './components/CreateLog'
+
 export default {
   name: 'app',
+  data() {
+    return {
+      logs: []
+    } 
+  },
   components: {
-    HelloWorld
+    CreateLog,
+  },
+  methods: {
+    onCreateLog: function(log) {
+      this.logs.push(log)
+
+    }
   }
 }
 </script>
---
+++ src/components/CreateLog.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <form @submit.prevent="submit">
+      <input type="text" v-model="content" />
+      <input type="submit" value="Add" v-if="content" />
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "CreateLog",
+  data() {
+    return {
+      content: null
+    };
+  },
+  methods: {
+    submit: function () {
+      this.$emit('create', this.content)
+    }
+  }
+};
+</script>
