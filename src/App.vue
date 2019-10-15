60fdca1b73f0f4a8234175a1387d9c568f2a5ac2
Alexandre Rousseau <contact@rousseau-alexandre.fr>
2019-10-15 23:11:31 +0200

    Improve UX

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <h1>Mind Dump</h1>
     <p>Export all your thoughts and let Natural Language Processing <a href="https://spacy.io/" rel="nofollow">Spacy</a> organize them.</p>
     <CreateLog @create="onCreateThought"/>
+    <div class="loader" v-if="loading"></div>
     <hr>
     <div class="column" v-for="(thought, index) in thoughts" :key="index">
       <Thought
@@ -28,7 +29,8 @@ export default {
   name: 'app',
   data() {
     return {
-      thoughts: []
+      thoughts: [],
+      loading: false
     } 
   },
   components: {
@@ -40,9 +42,11 @@ export default {
   },
   methods: {
     loadThoughts: function() {
+      this.loading = true
       axios.get('http://127.0.0.1:5000/thoughts/')
         .then(res => this.thoughts = res.data)
-        // .catch(e => console.error(e))
+        .catch(e => console.error(e))
+        .finally(() => this.loading = false)
     },
     onCreateThought: function(content) {
       const data = new FormData();
@@ -54,3 +58,62 @@ export default {
   }
 }
 </script>
+<style>
+.loader,
+.loader:before,
+.loader:after {
+  border-radius: 50%;
+  width: 2.5em;
+  height: 2.5em;
+  -webkit-animation-fill-mode: both;
+  animation-fill-mode: both;
+  -webkit-animation: load7 1.8s infinite ease-in-out;
+  animation: load7 1.8s infinite ease-in-out;
+}
+.loader {
+  color: #ffffff;
+  font-size: 10px;
+  margin: 80px auto;
+  position: relative;
+  text-indent: -9999em;
+  -webkit-transform: translateZ(0);
+  -ms-transform: translateZ(0);
+  transform: translateZ(0);
+  -webkit-animation-delay: -0.16s;
+  animation-delay: -0.16s;
+}
+.loader:before,
+.loader:after {
+  content: '';
+  position: absolute;
+  top: 0;
+}
+.loader:before {
+  left: -3.5em;
+  -webkit-animation-delay: -0.32s;
+  animation-delay: -0.32s;
+}
+.loader:after {
+  left: 3.5em;
+}
+@-webkit-keyframes load7 {
+  0%,
+  80%,
+  100% {
+    box-shadow: 0 2.5em 0 -1.3em;
+  }
+  40% {
+    box-shadow: 0 2.5em 0 0;
+  }
+}
+@keyframes load7 {
+  0%,
+  80%,
+  100% {
+    box-shadow: 0 2.5em 0 -1.3em;
+  }
+  40% {
+    box-shadow: 0 2.5em 0 0;
+  }
+}
+</style>
